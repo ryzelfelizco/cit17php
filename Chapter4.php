13f22af97493ef16e8a5b0eb742574e770f5dfa1
ryzelfelizco <ryzelerinfelizco@gmail.com>
2023-10-05 18:05:38 +0800

    Chapter 4

---
+++ Chapter4.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <?php
+
+        // Integers
+        $int_var = 12345;
+        $another_int = -12345 + 12345;
+        print("INTEGERS<br>");
+        print("$int_var<br>");
+        print("$another_int<br>");
+        
+        // Doubles
+        $many = 2.2888800;
+        $many_2 = 2.21112000;
+        $few = $many + $many_2;
+        print("<br>DOUBLES<br>");
+        print("$many + $many_2 = $few<br>");
+        ?>
+    </body>
+</html>
